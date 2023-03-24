2bdb358b4b511da4d41e57055f7e2a122edbd9e2
Davidentt <tsgoev.david.s@gmail.com>
2023-03-24 21:26:57 +0300

    Частично изменена инициализация View элементов в главном окне. Добавлено окно парной игры и реализовано получение данных. В LocalGameActivity.kt изменена сигнатура реализации подключения к устройству и начало игры. Подключени осуществляется по переходу в activity.

--- app/src/main/java/com/example/bluetooth_kotlin/LocalGameActivity.kt
+++ app/src/main/java/com/example/bluetooth_kotlin/LocalGameActivity.kt
@@ -1,68 +1,66 @@
 package com.example.bluetooth_kotlin
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
-import com.example.bt_def.BaseActivity
 import com.example.bt_def.BluetoothConstants
 import com.example.bt_def.BtConnection
 import com.example.bt_def.bluetooth.ReceiveThread
 
-class MainActivity : AppCompatActivity(), ReceiveThread.ListenerData {
+class LocalGameActivity : AppCompatActivity(), ReceiveThread.ListenerData {
     private var preferences: SharedPreferences? = null
     private var bAdapter: BluetoothAdapter? = null
     lateinit var btConnection: BtConnection
     private var gameFlag: Boolean = false
-    private val gameListPl1 = mutableListOf<Int>()
-    private val gameListPl2 = mutableListOf<Int>()
-    private lateinit var ibLocal: ImageButton
+    private var gameListPl1 = mutableListOf<Int>()
+    private var gameListPl2 = mutableListOf<Int>()
+    private lateinit var btStart: Button
+    private lateinit var tvPl1: TextView
+    private lateinit var tvPl2: TextView
 
     private fun initView(){
-        ibLocal = findViewById(R.id.ibLocal)
+        tvPl1 = findViewById(R.id.tvPl1)
+        tvPl2 = findViewById(R.id.tvPl2)
+        btStart = findViewById(R.id.btStart)
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, BaseActivity::class.java))
-
-        val button: Button = findViewById(R.id.btTest)
-        button.setOnClickListener { testPreferences() }
-
-        val buttonSendA: Button = findViewById(R.id.buttonSendA)
-        val buttonSendB: Button = findViewById(R.id.buttonSendB)
-        buttonSendA.setOnClickListener { btConnection.sendMessage("A") }
-        buttonSendB.setOnClickListener { btConnection.sendMessage("B") }
+        setContentView(R.layout.activity_local_game)
 
         initView()
-        //val ibLocal: ImageButton = findViewById(R.id.ibLocal)
-        ibLocal.setOnClickListener { startActivity(Intent(this ,LocalGameActivity::class.java)) }
-
         initBtAdapter()
         preferences =
             this.getSharedPreferences(BluetoothConstants.PREFERENCES, Context.MODE_PRIVATE)
 
+        btStart.setOnClickListener { startGame() }
+
+        connectToDevice()
     }
 
-    private fun testPreferences() {
+    private fun connectToDevice() {
         var mac = preferences?.getString(BluetoothConstants.MAC, "шш")
         Log.d("Debugging", "Test Preferences = $mac")
         if (mac != null) {
             btConnection.connect(mac)
         } else {
             Toast.makeText(this, "Устройство не выбрано!", Toast.LENGTH_LONG).show()
+            btStart.isEnabled = false
         }
         val myToast = Toast.makeText(this, "MAC: $mac", Toast.LENGTH_SHORT)
         myToast.show()
+    }
+
+    private fun startGame() {
+        btConnection.sendMessage("A")
     }
 
     private fun initBtAdapter() {
@@ -73,13 +71,13 @@ class MainActivity : AppCompatActivity(), ReceiveThread.ListenerData {
 
     override fun onReceive(message: String) {
         runOnUiThread {
-            val tView: TextView = findViewById(R.id.textView)
-            tView.text = message
+            tvPl1.text = message
             when (message) {
                 "START!" -> gameFlag = true
                 "FINISH!" -> {
                     gameFlag = false
-                    tView.text = "Your result is: ${this.gameListPl1.average().toInt()}"
+                    tvPl1.text = "Your result is: ${this.gameListPl1.average().toInt()}"
+                    tvPl2.text = "Your result is: ${this.gameListPl2.average().toInt()}"
 //                    Log.d("Debugging", "Check sum: ${gameList.sumOf { it / 10 }}")
                     Log.d("Debugging", "Check sum player1: ${this.gameListPl1.sum()}")
                     Log.d("Debugging", "Check sum player2: ${this.gameListPl2.sum()}")
@@ -88,7 +86,7 @@ class MainActivity : AppCompatActivity(), ReceiveThread.ListenerData {
                 }
             }
             if (gameFlag && message != "START!") {
-                if(message.first() == '<' && message.last() == '>') {
+                if (message.first() == '<' && message.last() == '>') {
                     var msg = message.removePrefix("<").removeSuffix(">")
                     gameListPl1.add(msg.substringBefore(',').toInt())
                     gameListPl2.add(msg.substringAfter(',').toInt())
